ff1d88cb7c2078649b7ab3fd070825ed2ddc0fcb
Mike McClurg <mike.mcclurg@citrix.com>
2013-01-05 12:17:06 +0000

    setup.sh now reads sudo files, for links that need sudo
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

--- setup.sh
+++ setup.sh
@@ -15,3 +15,16 @@ for f in $(find -name links); do
     ln -sf ${file} ${link}
   done
 done
+
+# Lazy copy-paste for sudo links
+for f in $(find -name sudo); do
+  d=$(pwd)/$(dirname $f)
+  #echo "dir: $d"
+  cat $f | while read line; do
+    file=${d}/$(echo ${line} | cut -d\  -f1)
+    link=$(echo ${line} | cut -d\  -f2)
+    link=$(eval echo ${link}) # to expand ~
+    echo "Linking ${link} -> ${file}"
+    sudo ln -sf ${file} ${link}
+  done
+done
