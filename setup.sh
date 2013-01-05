f27d3485d395a0b16384928f7b473dc9747731d6
Mike McClurg <mike.mcclurg@citrix.com>
2013-01-05 11:09:18 +0000

    Add setup.sh script to create symlinks
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This file parses files named "links", which have the format:
+# <filename> <link location>
+# We then create a link to each conf file in the appropriate location.
+
+for f in $(find -name links); do
+  d=$(pwd)/$(dirname $f)
+  #echo "dir: $d"
+  cat $f | while read line; do
+    file=${d}/$(echo ${line} | cut -d\  -f1)
+    link=$(echo ${line} | cut -d\  -f2)
+    link=$(eval echo ${link}) # to expand ~
+    echo "Linking ${link} -> ${file}"
+    ln -sf ${file} ${link}
+  done
+done
